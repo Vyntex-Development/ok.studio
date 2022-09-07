214a438f6b0e2f2f8ba59ad1154cfd0c23e890c3
Aleksandra Vukusic <vukusic.aleksandra@gmail.com>
2022-09-07 16:29:53 +0200

    animation

--- components/layout/HomeHero.js
+++ components/layout/HomeHero.js
@@ -1,12 +1,28 @@
 import Image from "next/image";
 import classes from "./HomeHero.module.css";
+import gsap from "gsap";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 const HomeHero = () => {
+  const imgRef = useRef(null);
+  useEffect(() => {
+    const el = imgRef.current;
+    gsap.fromTo(
+      el,
+      { width: "42rem", height: "25rem" },
+      {
+        width: "100vw",
+        height: "100vh",
+        duration: 3,
+      }
+    );
+  }, []);
   return (
     <div className="container">
       <div className={classes.HeroWrapper}>
         <div className={classes.HeroSticky}>
-          <div className={classes.HeroImageWrapper}>
+          <div className={classes.HeroImageWrapper} ref={imgRef}>
             <div className={classes.Image}>
               <Image
                 layout="fill"
