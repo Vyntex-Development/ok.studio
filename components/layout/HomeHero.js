e8559bde34ffec114237c3c90a8407a4e985b91b
Aleksandra Vukusic <vukusic.aleksandra@gmail.com>
2022-09-08 11:23:16 +0200

    hero animation done

--- components/layout/HomeHero.js
+++ components/layout/HomeHero.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import classes from "./HomeHero.module.css";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
+
 gsap.registerPlugin(ScrollTrigger);
 import { useRef } from "react";
 import { useEffect } from "react";
@@ -11,23 +12,47 @@ const HomeHero = () => {
   const imgRef = useRef(null);
   useEffect(() => {
     const el = imgRef.current;
-    const target = ".HeroImageWrapper";
-    gsap.fromTo(
-      el,
-      { width: "42rem", height: "25rem" },
+    const target = "#moving";
+
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: el,
+        // trigger element - viewport
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1,
+      },
+    });
+    tl.fromTo(
+      target,
+      {
+        width: "42rem",
+        height: "25rem",
+        duration: 1,
+      },
       {
         width: "100vw",
         height: "100vh",
-        duration: 3,
-        scrollTrigger: { trigger: target },
+        duration: 1,
       }
     );
+
+    // gsap.fromTo(
+    //   el,
+    //   { width: "42rem", height: "25rem" },
+    //   {
+    //     width: "100vw",
+    //     height: "100vh",
+    //     duration: 3,
+    //     scrollTrigger: { trigger: el },
+    //   }
+    // );
   }, []);
   return (
     <div className="container">
-      <div className={classes.HeroWrapper}>
-        <div className={classes.HeroSticky} ref={imgRef}>
-          <div className={classes.HeroImageWrapper}>
+      <div className={classes.HeroWrapper} ref={imgRef}>
+        <div className={classes.HeroSticky}>
+          <div className={classes.HeroImageWrapper} id="moving">
             <div className={classes.Image}>
               <Image
                 layout="fill"
