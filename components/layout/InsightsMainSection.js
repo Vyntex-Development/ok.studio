b037176e799aab54a04a6c5b6a0797c860d2aa65
Aleksandra Vukusic <vukusic.aleksandra@gmail.com>
2022-09-15 10:07:34 +0200

    insights items done

--- components/layout/InsightsMainSection.js
+++ components/layout/InsightsMainSection.js
@@ -1,9 +1,29 @@
+import Link from "../UI/Link";
+import classes from "./InsightsMainSection.module.css";
+import Image from "next/image";
+import InsightsItem from "./InsightsItem";
+
 const InsightsMainSection = () => {
   return (
     <div className="container">
       <div className="dotWrapperAbout">
         <div className="dot"></div>
         <p className="gray">Those who always stay invovled</p>
+      </div>
+      <div className={classes.ItemWrapper}>
+        <InsightsItem></InsightsItem>
+        <InsightsItem></InsightsItem>
+        <InsightsItem></InsightsItem>
+      </div>
+      <div className={classes.ItemWrapperStatic}>
+        <InsightsItem></InsightsItem>
+        <InsightsItem></InsightsItem>
+        <div>Yelow</div>
+      </div>
+      <div className={classes.ItemWrapper}>
+        <InsightsItem></InsightsItem>
+        <InsightsItem></InsightsItem>
+        <InsightsItem></InsightsItem>
       </div>
     </div>
   );
