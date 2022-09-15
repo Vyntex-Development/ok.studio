2225034200ebe48e56122fe49e6a7d65b953747a
Aleksandra Vukusic <vukusic.aleksandra@gmail.com>
2022-09-15 10:50:55 +0200

    insights desktop

--- components/layout/InsightsMainSection.js
+++ components/layout/InsightsMainSection.js
@@ -18,12 +18,34 @@ const InsightsMainSection = () => {
       <div className={classes.ItemWrapperStatic}>
         <InsightsItem></InsightsItem>
         <InsightsItem></InsightsItem>
-        <div>Yelow</div>
+        <div className="yellow-card">
+          <h1 className="h1-card">Subscribe</h1>
+          <p className="p-card">
+            Never stop learning. Subcribe to our newletter and get weekly
+            insights, strategies and case studies.
+          </p>
+          <form method="get">
+            <div className="input-wrapper">
+              <input type="email" placeholder="Email Address" required />
+              <button type="submit" className="submit-button">
+                SUBSCRIBE
+              </button>
+            </div>
+          </form>
+          <p className="p-card-last">
+            By clicking 'Subscribe' above, you agree to our Privacy Policy.
+          </p>
+        </div>
       </div>
       <div className={classes.ItemWrapper}>
         <InsightsItem></InsightsItem>
         <InsightsItem></InsightsItem>
         <InsightsItem></InsightsItem>
+      </div>
+      <div className={classes.ButtonWrapper}>
+        <Link href="/" type="dark">
+          LOAD MORE
+        </Link>
       </div>
     </div>
   );
