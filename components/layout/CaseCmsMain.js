86c11fbf11c3cdbbe56cd0cadcf0a67b22bd7eff
Aleksandra Vukusic <vukusic.aleksandra@gmail.com>
2022-09-19 15:51:26 +0200

    main

--- components/layout/CaseCmsMain.js
+++ components/layout/CaseCmsMain.js
@@ -1,3 +1,6 @@
+import classes from "./CaseCmsMain.module.css";
+import Image from "next/image";
+
 const CaseCmsMain = () => {
   return (
     <div>
@@ -6,9 +9,18 @@ const CaseCmsMain = () => {
           <div className="dot"></div>
           <p>What makes us totally ok</p>
         </div>
-        <div className="grid">
-          <div className="coll-9">coll8</div>
+        <div className={`${classes.Grid} grid`}>
           <div className="coll-3">coll4</div>
+          <div className="coll-9">
+            <div className={classes.Image}>
+              <Image
+                layout="fill"
+                objectFit="cover"
+                alt="hero"
+                src="/images/case_02.png"
+              ></Image>
+            </div>
+          </div>
         </div>
       </div>
     </div>
