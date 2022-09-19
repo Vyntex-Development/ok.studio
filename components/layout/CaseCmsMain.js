79c3a9b41c9b40fde2716207dcf2426a80a81d0f
Aleksandra Vukusic <vukusic.aleksandra@gmail.com>
2022-09-19 17:32:41 +0200

    solution section

--- components/layout/CaseCmsMain.js
+++ components/layout/CaseCmsMain.js
@@ -5,37 +5,40 @@ const CaseCmsMain = () => {
   return (
     <div>
       <div className="container">
-        <div className="dotWrapper">
-          <div className="dot"></div>
-          <p>What makes us totally ok</p>
-        </div>
-        <div className={`${classes.Grid} grid`}>
-          <div className="coll-3">
-            <div className={classes.ContentWrapper}>
-              <h2>OVERVIEW</h2>
-              <div className={classes.DescriptionWrapper}>
-                <p>
-                  We were very excited to work with Minespider. This innovative
-                  company has received funds from the European Union and has
-                  worked with industry giants such as Google, Volkswagen, and
-                  more.
-                </p>
-                <p>
-                  Minespider contacted us to create a new digital identity and
-                  website for their company. We love what they do and align with
-                  their brand values which are transparency and responsibility.
-                </p>
+        <div className={classes.OverviewSection}>
+          <div className="dotWrapper">
+            <div className="dot"></div>
+            <p>What makes us totally ok</p>
+          </div>
+          <div className={`${classes.Grid} grid`}>
+            <div className="coll-3">
+              <div className={classes.ContentWrapper}>
+                <h2>OVERVIEW</h2>
+                <div className={classes.DescriptionWrapper}>
+                  <p>
+                    We were very excited to work with Minespider. This
+                    innovative company has received funds from the European
+                    Union and has worked with industry giants such as Google,
+                    Volkswagen, and more.
+                  </p>
+                  <p>
+                    Minespider contacted us to create a new digital identity and
+                    website for their company. We love what they do and align
+                    with their brand values which are transparency and
+                    responsibility.
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="coll-9">
-            <div className={classes.Image}>
-              <Image
-                layout="fill"
-                objectFit="cover"
-                alt="hero"
-                src="/images/case_02.png"
-              ></Image>
+            <div className="coll-9">
+              <div className={classes.Image}>
+                <Image
+                  layout="fill"
+                  objectFit="cover"
+                  alt="hero"
+                  src="/images/case_02.png"
+                ></Image>
+              </div>
             </div>
           </div>
         </div>
@@ -192,6 +195,96 @@ const CaseCmsMain = () => {
                     <div className={classes.Tech}></div>
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.ChallengeSection}>
+          <div className="dotWrapper">
+            <div className="dot"></div>
+            <p>What makes us totally ok</p>
+          </div>
+          <div className={`${classes.ChallengeGrid} grid`}>
+            <div className={classes.ImageChallenge}>
+              <Image
+                layout="fill"
+                objectFit="cover"
+                alt="hero"
+                src="/images/challenge_01.png"
+              ></Image>
+            </div>
+            <div className={classes.ImageChallenge}>
+              <Image
+                layout="fill"
+                objectFit="cover"
+                alt="hero"
+                src="/images/challenge_02.png"
+              ></Image>
+            </div>
+            <div className={classes.ImageChallenge}>
+              <Image
+                layout="fill"
+                objectFit="cover"
+                alt="hero"
+                src="/images/challenge_03.png"
+              ></Image>
+            </div>
+          </div>
+          <div className={classes.ContentWrapperChallenge}>
+            <div className={classes.ContentWrapper}>
+              <h2>OVERVIEW</h2>
+              <div className={classes.DescriptionWrapper}>
+                <p>
+                  Minespider was not happy with its current digital image and
+                  needed to add marketing solutions for its potential customers.
+                  The challenge was to create a visual language that would
+                  integrate the mineral industry and blockchain technology by
+                  designing a unique and modern user experience.
+                </p>
+                <p>
+                  The plan included detailed service pages that served as
+                  landing pages with an appointment scheduling element, a modern
+                  blog, and a stable marketing infrastructure.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.SolutionSection}>
+          <div className="dotWrapper">
+            <div className="dot"></div>
+            <p>What makes us totally ok</p>
+          </div>
+          <div className={`${classes.SolutionGrid} grid`}>
+            <div className={`${classes.SolutionCollFour} coll-4`}>
+              <div className={classes.ContentWrapper}>
+                <h2>OUR SOLUTION</h2>
+                <div className={classes.DescriptionWrapper}>
+                  <p>
+                    In order to achieve and go beyond our client is
+                    expectations, we started by characterizing the work. We
+                    learned about the target audience and researched the market
+                    and their competitors.
+                  </p>
+                  <p>
+                    Once the characterization was stable and met customer
+                    expectations, we started building the initial impression of
+                    the company. The first impression is the most critical step
+                    in creating the brand image. We went through potential color
+                    palettes that work well with minerals and the world of
+                    advanced technology.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className={`${classes.SolutionCollEight} coll-8`}>
+              <div className={classes.Image}>
+                <Image
+                  layout="fill"
+                  objectFit="cover"
+                  alt="hero"
+                  src="/images/case_03.png"
+                ></Image>
               </div>
             </div>
           </div>
