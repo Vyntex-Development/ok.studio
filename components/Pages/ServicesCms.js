f56c8e3b76d24cb9fb26fe6a57320a962b6f1f99
Aleksandra Vukusic <vukusic.aleksandra@gmail.com>
2022-09-26 10:56:17 +0200

    faq services

--- components/Pages/ServicesCms.js
+++ components/Pages/ServicesCms.js
@@ -1,10 +1,20 @@
 import HeroServicesCms from "../layout/HeroServicesCms";
 
+import { useState } from "react";
+import FaqServices from "../layout/FaqServices";
+
 const ServicesCms = () => {
+  const [activeFaq, setActiveFaq] = useState("1");
   return (
     <div>
       <div>
         <HeroServicesCms></HeroServicesCms>
+        <FaqServices
+          activeFaq={activeFaq}
+          setActiveFaq={(id) => {
+            setActiveFaq(id);
+          }}
+        ></FaqServices>
       </div>
     </div>
   );
